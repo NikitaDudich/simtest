5816009c762fdda32268899cafa6f28098d240cf
ndudich <ndudich@webim.ru>
2024-02-18 07:04:14 -0800

    Class changes

--- utils/classes.py
+++ utils/classes.py
@@ -1,7 +1,3 @@
-from dataclasses import dataclass
-
-
-@dataclass
 class Transaction:
     def __init__(self, transaction_type: str, date, amount: str = None):
         self.transaction_type = transaction_type
